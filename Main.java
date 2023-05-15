955d3881df89368073a6756f1035ae5af6c063af
Erik <erikhgm@gmail.com>
2023-05-15 21:32:17 +0000

    initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.Date;
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 
+ * 
+ */
+public class Main {
+  public static void main(String[] args) {
+    
+  }
+}
